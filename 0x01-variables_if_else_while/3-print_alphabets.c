50af88b490b57fc863d9eb2d11f67c65bd3657b0
Termin0 <abdo.yasser951@gmail.com>
2023-07-14 14:12:21 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - Entry point of the program.
+ *
+ *the program prints alphabets lowercase.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+	int n = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++
+	}
+	putchar ('\n')
+	return (0);
+}
