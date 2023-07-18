5036ace6c8b59f91f17f2b80ee38548ead9b973a
Termin0 <abdo.yasser951@gmail.com>
2023-07-18 11:26:32 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include"0-putchar.c"
+#include "main.h"
 
 /**
-*main - main function.
-*
-*Return:0
+*main -  prints _putchar, followed by a new line
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-	char h[] = "Holberton";
-	int s = sizeof(h);
-	int i;
-
-	for (i = 0; i < s; i++)
-	{
-		_putchar(h[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
